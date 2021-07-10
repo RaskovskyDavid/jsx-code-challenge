33bef10a955ec36279746b24ab99645a1d9be5ef
David Ariel Raskovsky <davidtucu@hotmail.com>
2021-07-10 15:55:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom";
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+const name = "David";
+const lastname = "Raskovsky";
 ReactDOM.render(
   <div>
-    <h1>My favourite Foods</h1>
+    <h1>{lastname + " " + name} favourite Foods</h1>
     <ul>
-      <li>Milanesa Sandwich</li>
-      <li>Pizza</li>
+      <li>Milanesa Sandwich {Math.floor(Math.random() * 5)} stars</li>
+      <li>{`${name} Pizza`}</li>
       <li>Locro</li>
     </ul>
   </div>,
